5ccb7ad60cb0a0d0ad5f9d9d71eb0570c59dc77e
hakanErgin <hakan.ergin@gmail.com>
2021-12-26 17:52:44 +0100

    detection lanes added to camera stream calibration script added calibration files too

--- cam_stream.py
+++ cam_stream.py
@@ -1,10 +1,16 @@
 import cv2 as cv
-import os
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp"
+from dt_apriltags import Detector
 
-cap = cv.VideoCapture("rtsp://localhost:8554/cam", cv.CAP_FFMPEG)
 
-# cap = cv.VideoCapture("rtsp://localhost:8554/cam")
+at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+
+cap = cv.VideoCapture("rtsp://192.168.1.33:8554/cam")
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -17,6 +23,12 @@ while True:
         break
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    tags = at_detector.detect(gray)
+    for tag in tags:
+        for idx in range(len(tag.corners)):
+            cv.line(frame, tuple(tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
+
+
     # Display the resulting frame
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
